0bb8aa09c1542e321b7a3aca507741dfdbf2cd20
stud <stud@10-600-05>
2024-09-26 11:11:17 +0300

    Добавьте файлы проекта.

---
+++ Shotgun-Roulette-Game-TelegramBot/Form1.cs
@@ -0,0 +1,10 @@
+namespace Shotgun_Roulette_Game_TelegramBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
