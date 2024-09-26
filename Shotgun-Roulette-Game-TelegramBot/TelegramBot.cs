c98f1a8626d322001596cbdb72346b318084a220
stud <elhin2005@mail.ru>
2024-09-26 11:24:53 +0300

    Добавлены необходимые классы и формы

---
+++ Shotgun-Roulette-Game-TelegramBot/TelegramBot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shotgun_Roulette_Game_TelegramBot
+{
+    internal class TelegramBot
+    {
+    }
+}
